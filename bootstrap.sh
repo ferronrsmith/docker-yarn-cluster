df2e91037691de5ba8d145c2cb8e98c1e71ab42c
ferron <ferronrsmith@gmail.com>
2015-07-14 12:47:03 -0400

    reduce replication

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,11 +12,16 @@ cd $HADOOP_PREFIX/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; 
 service sshd start
 
 if [[ $1 = "-namenode" || $2 = "-namenode" ]]; then
+  mkdir -p /opt/data/hdfs/namenode
+  chmod 700 /opt/data/hdfs/namenode
+  su root -c "/usr/local/hadoop/bin/hdfs namenode -format"
   $HADOOP_PREFIX/sbin/start-dfs.sh
   $HADOOP_PREFIX/sbin/start-yarn.sh
 fi
 
 if [[ $1 = "-datanode" || $2 = "-datanode" ]]; then
+  mkdir -p /opt/data/hdfs/datanode
+  chmod 700 /opt/data/hdfs/datanode
   $HADOOP_PREFIX/sbin/start-dfs.sh
 fi
 
